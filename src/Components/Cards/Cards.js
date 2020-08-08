a8e9b2a19823caf4d1d18e6b4274e5a576ba87d8
deepak_shrivastava <deepak.shrivastava@marktine.com>
2020-08-08 08:26:11 +0530

    Integrated search with json keywords

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,82 +1,84 @@
-import React, {Component} from 'react'
-import { Button, Card, Image } from 'semantic-ui-react'
-import user from '../../Images/user.png';
-import 'font-awesome/css/font-awesome.min.css';
-import data from '../../Components/Data/users.json';
+import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
+import user from "../../Images/user.png";
+import "font-awesome/css/font-awesome.min.css";
 
+export default class Cards extends Component {
+  render() {
+    const { index, a } = this.props;
 
+    return (
+      <div key={index} style={{ height: "200px" }}>
+        <Card.Group>
+          <Card>
+            <Card.Content>
+              <div className="container-fluid">
+                <div id="cordMainRow" className="row shadow bg-white rounded">
+                  <div className="col-lg-2">
+                    <p id="assignedTo">Assigned To</p>
+                    <img id="cardImage" src={user} alt="" />
+                    <p id="userName">{a.name}</p>
+                    <p id="designation">{a.position}</p>
+                  </div>
 
-export default class Cards extends Component{
-  constructor(props){
-      super(props);
-      this.state = {
-             userDetails: data
-      }
-      console.log(this.state.userDetails);
-  }
-    
+                  <div className="offset-md-1 col-lg-3">
+                    <h6 id="cardTitle">Daily Scrum Meeting</h6>
+                    <p id="dueBy">
+                      Due by : <span id="dueDate">{a.dueBy}</span>
+                    </p>
+                    <br />
+                    <p id="dueBy">
+                      Meeting : <span id="dueDate">{a.meeting}</span>
+                    </p>
+                  </div>
 
-onChange = (e) => {
-  this.setState({ [e.target.id]: e.target.value });
-}
-
-onSubmit = (e) => {
-  e.preventDeafult();
-};
-
-render() {
-
-    return(
-      <div>
-         {this.state.userDetails.map((a, index) => (
-            <div key={index} style={{ height: "200px" }}>
-      <Card.Group>
-      <Card>
-        <Card.Content>
-          <div className = "container-fluid">
-              <div id = "cordMainRow" className = "row shadow bg-white rounded">
-                <div className = "col-lg-2">
-                  <p id = "assignedTo">Assigned To</p>
-                  <img id = "cardImage" src ={user}  alt = "" />
-                  <p id = "userName">{a.name}</p>
-                  <p id = "designation">{a.position}</p>
-                </div>
-  
-                <div className = "offset-md-1 col-lg-3">
-                  <h6 id = "cardTitle">Daily Scrum Meeting</h6>
-                  <p id = "dueBy">Due by : <span id = "dueDate">{a.dueBy}</span></p>
-                  <br />
-                  <p id = "dueBy">Meeting : <span id = "dueDate">{a.meeting}</span></p>
-                </div>
-  
-                <div className = "col-lg-1">
-                  <button id = "high" type = "button">High</button>
-                </div>
-                <div id = "rightColumn" className = "col-lg-4">
-                  <button id = "status" type = "button">Active</button>
-                  <div id = "CardIcons" className = "row">
-                    <div className = "col-sm-2">
-                      <span><i className = "fa fa-trash" style = {{fontSize: '18px', color: 'skyblue'}}></i></span>
-                    </div>
-                    <div className = "col-sm-2">
-                      <span><i className = "fa fa-edit" style = {{fontSize: '18px', color: 'skyblue'}}></i></span>
-                    </div>
-                    <div className = "col-sm-2">
-                      <span><i className = "fa fa-bell" style = {{fontSize: '18px', color: 'skyblue'}}></i></span>
-                    </div>
-                    <div className = "col-sm-6">
-                      <button id = "viewDetails" type = "button">View Details</button>
+                  <div className="col-lg-1">
+                    <button id="high" type="button">
+                      High
+                    </button>
+                  </div>
+                  <div id="rightColumn" className="col-lg-4">
+                    <button id="status" type="button">
+                      Active
+                    </button>
+                    <div id="CardIcons" className="row">
+                      <div className="col-sm-2">
+                        <span>
+                          <i
+                            className="fa fa-trash"
+                            style={{ fontSize: "18px", color: "skyblue" }}
+                          ></i>
+                        </span>
+                      </div>
+                      <div className="col-sm-2">
+                        <span>
+                          <i
+                            className="fa fa-edit"
+                            style={{ fontSize: "18px", color: "skyblue" }}
+                          ></i>
+                        </span>
+                      </div>
+                      <div className="col-sm-2">
+                        <span>
+                          <i
+                            className="fa fa-bell"
+                            style={{ fontSize: "18px", color: "skyblue" }}
+                          ></i>
+                        </span>
+                      </div>
+                      <div className="col-sm-6">
+                        <button id="viewDetails" type="button">
+                          View Details
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-          </div>
-        </Card.Content>
-      </Card>
-    </Card.Group>
-    </div>
-  ))}
-  </div>
-  );
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </div>
+    );
+  }
 }
-};
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,91 +1,128 @@
-import React, {Component} from 'react';
-import { render } from '@testing-library/react';
-import {  Link } from "react-router-dom";
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import 'font-awesome/css/font-awesome.min.css';
-import Cards from '../Components/Cards/Cards';
-import RecentActivity from '../Components/Cards/RecentActivity';
+import "font-awesome/css/font-awesome.min.css";
+import Cards from "../Components/Cards/Cards";
+import RecentActivity from "../Components/Cards/RecentActivity";
+import data from "../Components/Data/users.json";
 
-
-
-
-export default class Home extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-                month: '23 July 2020'
-        }
-    }
-
-
-onChange = (e) => {
-    this.setState({ [e.target.id]: e.target.value });
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userDetails: data
+    };
   }
 
-onSubmit = (e) => {
-    e.preventDeafult();
-  };
- 
   render() {
+    const handleSearchChange = e => {
+      const searchInput = e.target.value;
+      const allUsers = this.state.userDetails;
 
-      return(
-        <div className = "container-fluid">
-            <div className = "row">
-                <div id = "sideMenu" className = "col-lg-2 shadow p-3 mb-5 bg-white rounded">
-                    <button className ="Newtask"><FontAwesomeIcon icon={faPlus}/>&nbsp;New Task</button>
-                    <br />
-                    <Link to = "/meetings" ><i className="fa fa-meetup" ></i> Meetings</Link>
-                    <br />
-                    <br />
-                    <Link to = "/Tasks" ><i className = "fa fa-tasks"></i> Tasks</Link>
-                    <br />
-                    <br />
-                    <Link to = "/Teams" ><i className = "fa fa-users"></i> Teams</Link>
-                    <br />
-                    <br />
-                    <Link to = "/Help" ><i className = "fa fa-question-circle"></i> Help</Link>
-                    <br />
-                    <br />
-                    <Link to = "/Settings" ><i className = "fa fa-cog"></i> Settings</Link>
-                </div>
-                    <div className = "col-lg-10">
-                            <div className = "row">
-                                <div className = "col-sm-5">
-                                    <button className = "upcommingTask shadow  bg-white rounded">Upcomming Tasks</button>
-                                    <button className = "completed shadow  bg-white rounded">Completed</button>
-                                </div>
-                                <div className = "offset-sm-4 col-sm-3">
-                                <span id = "search"><i className = "fa fa-search"></i></span>
-                                <input className = "search shadow  bg-white rounded" placeholder = "Search ..." type = "search"  /> 
-                                </div>
-                            </div>
+      const filteredUsers = allUsers.filter(user => {
+        return user.name.toLowerCase().includes(searchInput.toLowerCase()) || user.position.toLowerCase().includes(searchInput.toLowerCase());
+      });
 
-                            <div id = "date" className = "row">
-                                <div className = "col-sm-2">
-                                    <div className = "row">
-                                        <div className = "col-xs-5">
-                                        <i class="fa fa-calendar" style = {{fontSize: '34px'}}></i>
-                                        </div>
-                                        <div id = "taskdate" className = "offset-xs-1 col-xs-6">
-                                            <p id = "month" style = {{color: 'red', fontSize: '10px'}}>{this.state.month}</p>
-                                            <p id = "todaytask" style = {{fontSize: '14px', fontWeight: '500'}}>Today's Tasks</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+      searchInput
+        ? this.setState({ userDetails: filteredUsers })
+        : this.setState({ userDetails: data });
+    };
 
-                            <div className = "row">
-                                <div className = "col-sm-8">
-                                    <Cards />
-                                </div>
-                                <div className = "col-sm-4">
-                                    <RecentActivity />
-                                </div>
-                            </div>
-                    </div>
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div
+            id="sideMenu"
+            className="col-lg-2 shadow p-3 mb-5 bg-white rounded"
+          >
+            <button className="Newtask">
+              <FontAwesomeIcon icon={faPlus} />
+              &nbsp;New Task
+            </button>
+            <br />
+            <Link to="/meetings">
+              <i className="fa fa-meetup"></i> Meetings
+            </Link>
+            <br />
+            <br />
+            <Link to="/Tasks">
+              <i className="fa fa-tasks"></i> Tasks
+            </Link>
+            <br />
+            <br />
+            <Link to="/Teams">
+              <i className="fa fa-users"></i> Teams
+            </Link>
+            <br />
+            <br />
+            <Link to="/Help">
+              <i className="fa fa-question-circle"></i> Help
+            </Link>
+            <br />
+            <br />
+            <Link to="/Settings">
+              <i className="fa fa-cog"></i> Settings
+            </Link>
+          </div>
+          <div className="col-lg-10">
+            <div className="row">
+              <div className="col-sm-5">
+                <button className="upcommingTask shadow  bg-white rounded">
+                  Upcoming Tasks
+                </button>
+                <button className="completed shadow  bg-white rounded">
+                  Completed
+                </button>
+              </div>
+              <div className="offset-sm-4 col-sm-3">
+                <span id="search">
+                  <i className="fa fa-search"></i>
+                </span>
+                <input
+                  className="search shadow  bg-white rounded"
+                  placeholder="Search ..."
+                  type="search"
+                  onChange={e => handleSearchChange(e)}
+                />
+              </div>
             </div>
+
+            <div id="date" className="row">
+              <div className="col-sm-2">
+                <div className="row">
+                  <div className="col-xs-5">
+                    <i class="fa fa-calendar" style={{ fontSize: "34px" }}></i>
+                  </div>
+                  <div id="taskdate" className="offset-xs-1 col-xs-6">
+                    <p id="month" style={{ color: "red", fontSize: "10px" }}>
+                      {this.state.month}
+                    </p>
+                    <p
+                      id="todaytask"
+                      style={{ fontSize: "14px", fontWeight: "500" }}
+                    >
+                      Today's Tasks
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm-8">
+                {this.state.userDetails.map((a, index) => (
+                  <Cards a={a} index={index} />
+                ))}
+              </div>
+              <div className="col-sm-4">
+                <RecentActivity />
+              </div>
+            </div>
+          </div>
         </div>
-      );
+      </div>
+    );
   }
-};
+}
